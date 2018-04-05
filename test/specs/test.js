539b36d6151e023c637adfc782097bc948763ff9
maryia.radkevich <maskaradm@gmail.com>
2018-04-05 19:58:19 +0300

    extend test

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,11 +1,33 @@
-var assert = require('assert');
+let assert = require('assert');
 
 describe('webdriver.io page', function () {
 
     it('should have the right title', function () {
         browser.url('/');
-        browser.click('=API');
-        var title = browser.getTitle();
-        assert.equal(title, "WebdriverIO - API Docs")
+        let title = browser.getTitle();
+        assert.equal(title, "WebdriverIO - WebDriver bindings for Node.js")
     });
+
+    describe('Api Page', function () {
+        it('should have a link to it from homepage', function () {
+            browser.url("/");
+            let hasApiLink = browser.isExisting('=API');
+            assert(hasApiLink)
+        });
+
+        it('should take you to the API page', function () {
+            browser.url("/");
+            browser.click('=API');
+            let title = browser.getTitle();
+            assert.equal(title, "WebdriverIO - API Docs")
+
+            // browser.debug();
+        });
+
+        it('should filter search results', function () {
+            browser.url("/api.html");
+            browser.setValue('[name="search"]', "debug");
+            browser.saveScreenshot("screen.png")
+        })
+    })
 });
