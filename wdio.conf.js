a7dad81b9438d9c0bcaa9a7b0cf8da8ede2652b4
maryia.radkevich <maskaradm@gmail.com>
2018-04-05 17:24:23 +0300

    config is changed

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,9 +1,9 @@
 var caps = {};
-caps['browserName'] = 'firefox';
-// caps['browserName'] = 'safari';
-// caps['platform'] = 'macOS 10.13';
-// caps['version'] = '11.0';
-// caps['screenResolution'] = '1024x768';
+// caps['browserName'] = 'firefox';
+caps['browserName'] = 'safari';
+caps['platform'] = 'macOS 10.13';
+caps['version'] = '11.0';
+caps['screenResolution'] = '1024x768';
 
 let config = {
     //
@@ -56,14 +56,7 @@ let config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [{
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'firefox'
-    }],
+    capabilities: [caps],
     //
     // ===================
     // Test Configurations
@@ -142,7 +135,13 @@ let config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/reporters/dot.html
-    reporters: ['spec'],
+    reporters: ['dot', 'junit'],
+
+    reporterOptions: {
+        junit: {
+            outputDir: './reports/'
+        }
+    },
 
     //
     // Options to be passed to Mocha.
